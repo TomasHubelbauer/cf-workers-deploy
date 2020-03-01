2f633aff620958bb4eee9813b3d428f874bdcd0e
Tomas Hubelbauer <tomas@hubelbauer.net>
2020-03-01 01:34:23 +0100

    Implement worker deployment without bindings

--- index.js
+++ index.js
@@ -1,50 +1,74 @@
+const secrets = require('./secrets');
+const crypto = require('crypto');
 const fs = require('fs-extra');
 const fetch = require('node-fetch');
 
 void async function () {
-  const { accountId, workerName, namespaceId, inheritFrom, xAtok, cookie } = require('./secrets');
-  const worker = await fs.readFile('worker.js', { encoding: 'ascii' });
+  // Find in https://dash.cloudflare.com/ (on load appended to the URL)
+  const accountId = secrets.accountId || process.argv[2];
+  if (!accountId) {
+    throw new Error('Account ID must be supplied in the accountId key of secrets.json or in the 1st command line argument.');
+  }
 
-  const data = {
+  const workerName = secrets.workerName || process.argv[3];
+  if (!workerName) {
+    throw new Error('Worker name must be supplied in the workerName key of secrets.json or in the 2nd command line argument.');
+  }
+
+  // Find in dashboard network requests' X-ATOK HTTP request headers in the dev tools
+  const accessToken = secrets.accessToken || process.argv[4];
+  if (!accessToken) {
+    throw new Error('Access token must be supplied in the accessToken key of secrets.json or in the 3rd command line argument.');
+  }
+
+  // Go to https://dash.cloudflare.com/${accountId}/workers/edit/${workerName}
+  // and find the `vses2` cookie value in the browser developer tools
+  const sessionCookie = secrets.sessionCookie || process.argv[4];
+  if (!sessionCookie) {
+    throw new Error('Session cookie must be supplied in the sessionCookie key of secrets.json or in the 4th command line argument.');
+  }
+
+  if (!await fs.pathExists('worker.js')) {
+    throw new Error('worker.js must exist');
+  }
+
+  const script = await fs.readFile('worker.js', { encoding: 'utf8' });
+
+  const metadata = {
     body_part: 'script',
-    bindings: [
-      { name: 'KV', type: 'kv_namespace', namespaceId },
-    ],
-    inherit_from: inheritFrom
+    bindings: [],
   };
 
-  const boundary = '---------------------------283571332918390';
+  const boundary = crypto.randomBytes(20).toString('hex');
   const body = [
-    boundary,
+    `--${boundary}`,
     'Content-Disposition: form-data; name="metadata"; filename="blob"',
     'Content-Type: application/json',
     '',
-    JSON.stringify(data),
-    '--' + boundary,
+    //'',
+    JSON.stringify(metadata),
+    `--${boundary}`,
     'Content-Disposition: form-data; name="script"; filename="blob"',
     'Content-Type: application/javascript',
     '',
-    worker,
-    '',
-    boundary + '--',
+    //'',
+    script,
+    `--${boundary}--`,
     ''
   ].join('\r\n');
 
-  const response = await fetch(`https://dash.cloudflare.com/api/v4/accounts/${accountId}/workers/scripts/${workerName}/preview`, {
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0',
-      Accept: '*/*',
-      'Accept-Language': 'en-US,en;q=0.5',
-      'Content-Type': 'multipart/form-data; boundary=' + boundary,
-      Cookie: cookie,
-      Pragma: 'no-cache',
-      'Cache-Control': 'no-cache',
-      'X-ATOK': xAtok
-    },
-    referrer: 'https://dash.cloudflare.com/',
-    body,
+  const response = await fetch(`https://dash.cloudflare.com/api/v4/accounts/${accountId}/workers/scripts/${workerName}`, {
     method: 'PUT',
+    headers: {
+      'X-ATOK': accessToken,
+      'Content-Type': `multipart/form-data; boundary=${boundary}`,
+      Cookie: `vses2=${sessionCookie}`
+    },
+    body,
   });
 
-  console.log(await response.text());
+  const data = await response.json();
+  if (!data.success) {
+    throw new Error('The API has responded with an error:\n' + JSON.stringify(data, null, 2));
+  }
 }()
--- readme.md
+++ readme.md
@@ -1,11 +1,56 @@
 # CloudFlare Workers Deploy
 
+## Installation
+
+`npm install https://github.com/TomasHubelbauer/cf-workers-deploy`
+
+## Usage
+
+```javascript
+const deploy = require('cf-worker-deploy');
+
+// Create secrets.json and worker.js - see the Running section of this readme
+await deploy();
+```
+
 ## Running
 
-Create `secrets.json` and `worker.js` first.
+Create `secrets.json` and `worker.js` first:
+
+`secrets.json`:
+```json
+// See the code comments in `index.js` for guidance on how to obtain these values
+{
+  "accountId": "…",
+  "workerName": "…",
+  "accessToken": "…-ATOK…",
+  "sessionCookie": "…" // Without the `vses2=` prefix
+}
+```
+
+`worker.js`:
+```javascript
+// Your worker code, e.g.:
+addEventListener('fetch', event => event.respondWith(handleRequest(event.request)));
+
+async function handleRequest(request) {
+  return new Response('hello world', { status: 200 });
+}
+```
 
 `node .`
 
+## Testing
+
+See *Running*.
+
+## Changelog
+
+### `1.0.0` 2020-03-01
+
+The initial release with support for deploying a worker with no bindings based
+on `secrets.json` with manually obtained values.
+
 ## To-Do
 
-### Figure out the cause of the *unexpected EOF* error and finalize the project
+### Add support for namespace bindings to enable preserving KV bindings
